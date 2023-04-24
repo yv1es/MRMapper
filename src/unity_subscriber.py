2ad1129f31ae4e32dce5abae4a3822432bbd6ebd
yv1es <yvesinglin@gmail.com>
2023-04-24 11:45:05 +0200

    Generic unity sender/receiver

--- src/unity_subscriber.py
+++ src/unity_subscriber.py
@@ -15,106 +15,97 @@ HOST = s.gethostname()
 PORT_PCL = 5001
 PORT_ODOM = 5002
 
-conn_pcl = None
-conn_odom = None
-sender_queue_pcl = queue.Queue()
-sender_queue_odom = queue.Queue()
-
-
-def setupSockets():
-    socket_pcl = s.socket(s.AF_INET, s.SOCK_STREAM)
-    socket_pcl.bind((HOST, PORT_PCL)) 
-    socket_pcl.listen()
-
-    socket_odom = s.socket(s.AF_INET, s.SOCK_STREAM)
-    socket_odom.bind((HOST, PORT_ODOM)) 
-    socket_odom.listen()
-
-    rospy.loginfo("Listening for Unity connection")
-    global conn_pcl, conn_odom
-    conn_pcl, _ = socket_pcl.accept()
-    conn_odom, _ = socket_odom.accept()
-
-
-
-
 
 # http://docs.ros.org/en/melodic/api/sensor_msgs/html/msg/PointCloud2.html
 def callback_cloud_map(cloud_map):   
     data = cloud_map.data
-    header = struct.pack('!I', len(data))
-    message = header + data
-    sender_queue_pcl.put(message)
+    sender_pcl.send(data)
     
-
-
 
 # http://docs.ros.org/en/noetic/api/nav_msgs/html/msg/Odometry.html
 def callback_odom(odom):
-    
-    position = odom.pose.pose.position # x, y, z
-    orientation = odom.pose.pose.orientation # quaternion x, y, z, w
-    
+    position = odom.pose.pose.position 
+    orientation = odom.pose.pose.orientation 
     data = np.array([position.x, position.y, position.z, orientation.x, orientation.y, orientation.z, orientation.w], dtype=np.float32).tobytes()
-    header = struct.pack('!I', len(data))
-    message = header + data
-    sender_queue_odom.put(message)
-
-def senderThreadPcl():
-    while True:
-        try:
-            data = sender_queue_pcl.get(timeout=1)
-            conn_pcl.sendall(data)
-            sender_queue_pcl.task_done()
-        except queue.Empty:
-            pass
-
-def senderThreadOdom():
-    while True:
-        try:
-            data = sender_queue_odom.get(timeout=1)
-            conn_odom.sendall(data)
-            sender_queue_odom.task_done()
-        except queue.Empty:
-            pass
-
-
-def main():
+    sender_odom.send(data)
     
-    # Initialize the node
+
+def main():    
     rospy.init_node('unity_subsciber', anonymous=True)
     
-    # establish connection 
-    setupSockets()
-    
+    global sender_pcl, sender_odom
+    sender_pcl = UnitySender(HOST, PORT_PCL, 'Pcl Sender')
+    sender_odom = UnitySender(HOST, PORT_ODOM, 'Odom Sender')
 
-    rospy.loginfo("Connected to unity")
-
-    # Subscribe to the "/rtabmap/cloud_map" topic
-    rospy.Subscriber("/rtabmap/cloud_map", PointCloud2, callback_cloud_map)
-
-    # Subscribe to the "/rtabmap/odom" topic
-    rospy.Subscriber("/rtabmap/odom", Odometry, callback_odom)
-
-    # Spin until the node is stopped
-    # rospy.spin()
-
-    sender_pcl = threading.Thread(target=senderThreadPcl)
-    sender_pcl.daemon = True
     sender_pcl.start()
-
-    sender_odom = threading.Thread(target=senderThreadOdom)
-    sender_odom.daemon = True
     sender_odom.start()
 
+    rospy.loginfo("Connected to unity")
+    
+    rospy.Subscriber("/rtabmap/cloud_map", PointCloud2, callback_cloud_map)
+    rospy.Subscriber("/rtabmap/odom", Odometry, callback_odom)
 
     rospy.spin()
+
+
+
+class UnitySender:
+    def __init__(self, ip, port, tag) -> None:        
+        self.ip = ip 
+        self.port = port 
+        self.tag = tag
+        self.sender_queue = queue.Queue()
+        self.running = True
+        self.conn = None
+        
+
+    def start(self):
+        self.socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+        self.socket.bind((self.ip, self.port)) 
+        self.socket.listen()
+        self.log("Listening incoming unity connection")
+        self.conn, _ = self.socket.accept()
+        self.log("Connected to unity")
+
+        self.sender = threading.Thread(target=self.senderThread)
+        self.sender.daemon = True
+        self.sender.start()
+        self.log("Sender thread running")
+
+
+    def log(self, message):
+        rospy.loginfo("[{}] {}".format(self.tag, message))
+
+
+    def senderThread(self):
+        while self.running:
+                try:
+                    data = self.sender_queue.get(timeout=1)
+                    self.conn.sendall(data)
+                    self.sender_queue.task_done()
+                except queue.Empty:
+                    pass
+
+
+    def send(self, data):
+        header = struct.pack('!I', len(data))
+        message = header + data
+        self.sender_queue.put(message)
+
+
+    def stop(self):
+        self.running = False
+        self.conn.close()
+
+
+
 
 if __name__ == '__main__':
     try:
         main()
     except rospy.ROSInterruptException:
-        pass
+        sender_pcl.stop()
+        sender_odom.stop()
 
 
 
