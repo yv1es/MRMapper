f3bbbda24914327c12319a74940434c117bb0c96
yv1es <yvesinglin@gmail.com>
2023-05-22 21:51:08 +0200

    basic plane segmentation

--- src/segmentation.py
+++ src/segmentation.py
@@ -8,6 +8,8 @@ from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge
 import socket as s 
 
+from unity_sender import UnitySender 
+
 import mxnet as mx
 from gluoncv import model_zoo, data
 import matplotlib.pyplot as plt
@@ -547,55 +549,6 @@ def main():
 
 
 
-
-class UnitySender:
-    def __init__(self, ip, port, tag) -> None:        
-        self.ip = ip 
-        self.port = port 
-        self.tag = tag
-        self.sender_queue = queue.Queue()
-        self.running = True
-        self.conn = None
-        
-
-    def start(self):
-        self.socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-        self.socket.bind((self.ip, self.port)) 
-        self.socket.listen()
-        self.log("Listening incoming unity connection")
-        self.conn, _ = self.socket.accept()
-        self.log("Connected to unity")
-
-        self.sender = threading.Thread(target=self.senderThread)
-        self.sender.daemon = True
-        self.sender.start()
-        self.log("Sender thread running")
-
-
-    def log(self, message):
-        rospy.loginfo("[{}] {}".format(self.tag, message))
-
-
-    def senderThread(self):
-        while self.running:
-                try:
-                    data = self.sender_queue.get(timeout=1)
-                    self.conn.sendall(data)
-                    self.sender_queue.task_done()
-                except queue.Empty:
-                    pass
-
-
-    def send(self, data):
-        header = struct.pack('!I', len(data))
-        message = header + data
-        self.sender_queue.put(message)
-
-
-    def stop(self):
-        self.running = False
-        self.conn.close()
-        self.socket.close()
 
 
 
